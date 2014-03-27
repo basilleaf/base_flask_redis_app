34d7b4228ef70632431afa310b1db212659bbb99
lisa ballard <lballard.cat@gmail.com>
2014-03-26 21:07:39 -0700

    adds redis heroku connection line to hello world script

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 import os
-from flask import Flask
+import redis
+from flask import Flask, render_template, redirect
+from util import json, jsonp
+from json import loads
+
+app = Flask(__name__)
+REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+r_server = redis.StrictRedis.from_url(REDIS_URL)
 
 app = Flask(__name__)
 
@@ -9,4 +16,5 @@ def hello():
 
 
 if __name__ == '__main__':
+    app.debug = False
     app.run()
